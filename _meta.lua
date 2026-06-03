6bcb01ae869baf27922ce723c32328116cdd594c
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2026-06-03 00:12:15 -0300

    chore: scaffold koremote plugin metadata and busted config

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+return {
+    name = "koremote",
+    fullname = "KO Remote",
+    description = "HTTP REST control API + UDP discovery so a companion app can read device/book info and remote-control KOReader.",
+    version = "v0.1.0",
+}
